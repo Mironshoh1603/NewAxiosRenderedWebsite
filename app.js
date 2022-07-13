b271a1ef3485f4acdf20cfd68c4f2b2f57c742e6
Mironshoh1603 <mironshohasadov1603.gmail.com>
2022-07-13 12:33:00 +0500

    finish

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// const app = express()
-// const port = 5000
+const app = express();
+const port = 5000;
 
-// // Static Files
-// app.use(express.static('public'))
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/img', express.static(__dirname + 'public/img'))
-// app.use('/js', express.static(__dirname + 'public/js'))
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use("/js", express.static(__dirname + "public/js"));
 
-// // Templating Engine
-// app.set('views', './src/views')
-// app.set('view engine', 'ejs')
+// Templating Engine
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// // Routes
-// const newsRouter = require('./src/routes/news')
+// Routes
+const newsRouter = require("./src/routes/news");
 
-// app.use('/', newsRouter)
-// app.use('/article', newsRouter)
+app.use("/", newsRouter);
+app.use("/article", newsRouter);
 
-// // Listen on port 5000
-// app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
+// Listen on port 5000
+app.listen(process.env.PORT || port, () =>
+  console.log(`Listening on port ${port}`)
+);
